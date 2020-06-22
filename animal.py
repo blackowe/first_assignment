6b2809cf60afe6011b9902eff6d2c06282dc8678
blackowe <blackowe@oregonstate.edu>
2020-06-22 16:05:14 -0500

    First commit attempt - Assignment1

---
+++ animal.py
@@ -0,0 +1,8 @@
+# Author: Erik Blackowicz
+# Date: 6/21/20
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal.
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
+
